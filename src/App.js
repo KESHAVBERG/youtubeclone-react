ff42764d336ba64f22a1588780f619c42988b797
KESHAVBERG <kingkeshav7@gmail.com>
2022-09-08 10:51:51 +0530

    working on navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Feed, VideoDetails, SearchFeed, ChannelDetails, NavBar } from './Components';
 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <NavBar />
+        <Routes>
+          < Route path='/' exact element={<Feed />} />
+          <Route path='/videos/:id' element={<VideoDetails />} />
+          <Route path='/channel/:id' element={<ChannelDetails />} />
+          <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import {React, useState} from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Paper, IconButton } from '@mui/material'
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+const SearchBar = () => {
+  return (
+    <div>SearchBar</div>
+  )
+}
+
+export default SearchBar
